7657ce7c671ead11257ad2d33e91377861880782
Adam Wayne <adamwayne.dev@gmail.com>
2022-10-08 00:17:03 +0100

    Started Adding Functionality for subgenres

--- Assets/Scripts/SubGenres.cs
+++ Assets/Scripts/SubGenres.cs
@@ -2,142 +2,165 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SubGenres : ScriptableObject
+
+public class SubGenres : MonoBehaviour
 {
+    
+    List<string> action_SubGenres = new List<string>();
+    List<string> adventure_SubGenres = new List<string>();
+    List<string> puzzle_SubGenres = new List<string>();
+    List<string> rpg_SubGenres = new List<string>();
+    List<string> simulation_SubGenres = new List<string>();
+    List<string> rhythm_SubGenres = new List<string>();
+    List<string> strategy_SubGenres = new List<string>();
+    List<string> sports_SubGenres = new List<string>();
+    List<string> casual_SubGenres = new List<string>();
 
-    public string[] action_SubGenres = new string[12];
-    public string[] adventure_SubGenres = new string[6];
-    public string[] puzzle_SubGenres = new string[10];
-    public string[] rpg_SubGenres = new string[10];
-    public string[] simulation_SubGenres = new string[5];
-    public string[] rhythm_SubGenres = new string[4];
-    public string[] strategy_SubGenres = new string[12];
-    public string[] sports_SubGenres = new string[6];
-    public string[] casual_SubGenres = new string[6];
-
-
-
-    public string[] Get_Action_Subgenres()
+    private void Start()
     {
-        action_SubGenres[0] = "Random";
-        action_SubGenres[1] = "Platform";
-        action_SubGenres[2] = "First-Person Shooter";
-        action_SubGenres[3] = "Third-Person Shooter";
-        action_SubGenres[4] = "Fighting";
-        action_SubGenres[5] = "Beat'em up";
-        action_SubGenres[6] = "Stealth";
-        action_SubGenres[7] = "Surviveal";
-        action_SubGenres[8] = "Battle Royale";
-        action_SubGenres[9] = "Horror";
-        action_SubGenres[10] = "Metroidvania";
-        action_SubGenres[11] = "Custom";
+        Add_Action_Subgenres();
+        Add_Adventure_Subgenres();
+        Add_Puzzle_Subgenres();
+        Add_RPG_Subgenres();
+    }
+
+    public void Add_Action_Subgenres()
+    {
+        action_SubGenres.Add("Random");
+        action_SubGenres.Add("Platform");
+        action_SubGenres.Add("First-Person Shooter");
+        action_SubGenres.Add("Third-Person Shooter");
+        action_SubGenres.Add("Fighting");
+        action_SubGenres.Add("Beat'em up");
+        action_SubGenres.Add("Stealth");
+        action_SubGenres.Add("Survival");
+        action_SubGenres.Add("Battle Royale");
+        action_SubGenres.Add("Horror");
+        action_SubGenres.Add("Metroidvania");
         
+    }
+
+    public List<string> Get_Action_Subgenres()
+    {
         return action_SubGenres;
     }
-    public string[] Get_Adventure_Subgenres()
+    public void Add_Adventure_Subgenres()
     {
-        adventure_SubGenres[0] = "Random";
-        adventure_SubGenres[1] = "Text Adventure";
-        adventure_SubGenres[2] = "Visual Novel";
-        adventure_SubGenres[3] = "Interactive Movie";
-        adventure_SubGenres[4] = "Real-Time 3D Adventure";
-        adventure_SubGenres[5] = "Custom";
-        
+        adventure_SubGenres.Add("Random");
+        adventure_SubGenres.Add("Text Adventure");
+        adventure_SubGenres.Add("Visual Novel");
+        adventure_SubGenres.Add("Interactive Movie");
+        adventure_SubGenres.Add("Real-Time 3D Adventure");
+        adventure_SubGenres.Add("Classic Adventure");
+    }
+    public List<string> Get_Adventure_Subgenres()
+    {
         return adventure_SubGenres;
     }
 
-    public string[] Get_Puzzle_Subgenres()
+    public void Add_Puzzle_Subgenres()
     {
-        puzzle_SubGenres[0] = "Random";
-        puzzle_SubGenres[1] = "Breakout Clone";
-        puzzle_SubGenres[2] = "Logical";
-        puzzle_SubGenres[3] = "Trial and Error";
-        puzzle_SubGenres[4] = "Hidden Object";
-        puzzle_SubGenres[5] = "Reveal the picture";
-        puzzle_SubGenres[6] = "Tile-Matching";
-        puzzle_SubGenres[7] = "Traditional";
-        puzzle_SubGenres[8] = "Puzzle-Platform";
-        puzzle_SubGenres[9] = "Custom";
-
+        puzzle_SubGenres.Add("Random");
+        puzzle_SubGenres.Add("Breakout Clone");
+        puzzle_SubGenres.Add("Logical");
+        puzzle_SubGenres.Add("Trial and error");
+        puzzle_SubGenres.Add("Hidden Object");
+        puzzle_SubGenres.Add("Reveal the Picture");
+        puzzle_SubGenres.Add("Tile-Matching");
+        puzzle_SubGenres.Add("Traditional");
+        puzzle_SubGenres.Add("Puzzle-Platform");
+    }
+    public List<string> Get_Puzzle_Subgenres()
+    {
         return puzzle_SubGenres;
     }
 
-    public string[] Get_RPG_Subgenres()
+    public void Add_RPG_Subgenres()
     {
-        rpg_SubGenres[0] = "Random";
-        rpg_SubGenres[1] = "Action RPG";
-        rpg_SubGenres[2] = "JRPG";
-        rpg_SubGenres[3] = "Roguelike";
-        rpg_SubGenres[4] = "Tactical RPG";
-        rpg_SubGenres[5] = "Sandbox RPG";
-        rpg_SubGenres[6] = "First Person RPG";
-        rpg_SubGenres[7] = "Monster Tamer";
-        rpg_SubGenres[8] = "Card RPG";
-        rpg_SubGenres[9] = "Custom";
-
+        rpg_SubGenres.Add("Random");
+        rpg_SubGenres.Add("Action RPG");
+        rpg_SubGenres.Add("JRPG");
+        rpg_SubGenres.Add("RogueLike");
+        rpg_SubGenres.Add("Tactical RPG");
+        rpg_SubGenres.Add("Sandbox RPG");
+        rpg_SubGenres.Add("First Person RPG");
+        rpg_SubGenres.Add("Monster Tamer");
+        rpg_SubGenres.Add("Card RPG");
+    }
+    public List<string> Get_RPG_Subgenres()
+    {
         return rpg_SubGenres;
     }
 
-    public string[] Get_Sim_Subgenres()
+    public void Add_Sim_Subgenres()
     {
-        simulation_SubGenres[0] = "Random";
-        simulation_SubGenres[1] = "Construction and Management";
-        simulation_SubGenres[2] = "Life Simulation";
-        simulation_SubGenres[3] = "Vehicle Simulation";
-        simulation_SubGenres[4] = "Custom";
-
-        return rpg_SubGenres;
+        simulation_SubGenres.Add("Random");
+        simulation_SubGenres.Add("Construction and Management");
+        simulation_SubGenres.Add("Life Simulation");
+        simulation_SubGenres.Add("Vehicle Simulation");
     }
 
-    public string[] Get_Rhythm_Subgenres()
+    public List<string> Get_Sim_Subgenres()
     {
-        rhythm_SubGenres[0] = "Random";
-        rhythm_SubGenres[1] = "Music";
-        rhythm_SubGenres[2] = "Dance";
-        rhythm_SubGenres[3] = "Custom";
+        return simulation_SubGenres;
+    }
 
+    public void Add_Rhythm_Subgenres()
+    {
+        rhythm_SubGenres.Add("Random");
+        rhythm_SubGenres.Add("Music");
+        rhythm_SubGenres.Add("Dance");
+    }
+
+    public List<string> Get_Rhythm_Subgenres()
+    {
         return rhythm_SubGenres;
     }
 
-    public string[] Get_Strategy_Subgenres()
+    public void Add_Strategy_Subgenres()
     {
-        strategy_SubGenres[0] = "Random";
-        strategy_SubGenres[1] = "4X";
-        strategy_SubGenres[2] = "Artillery";
-        strategy_SubGenres[3] = "Auto Battler";
-        strategy_SubGenres[4] = "Real-time Strategy";
-        strategy_SubGenres[5] = "Real-time Tactics";
-        strategy_SubGenres[6] = "Tower Defense";
-        strategy_SubGenres[7] = "Turn-based Strategy";
-        strategy_SubGenres[8] = "Turn-based Tactics";
-        strategy_SubGenres[9] = "Wargame";
-        strategy_SubGenres[10] = "Grand Strategy Wargame";
-        strategy_SubGenres[11] = "Custom";
-
+        strategy_SubGenres.Add("Random");
+        strategy_SubGenres.Add("4X");
+        strategy_SubGenres.Add("Artillery");
+        strategy_SubGenres.Add("Auto Battler");
+        strategy_SubGenres.Add("Real-time Strategy");
+        strategy_SubGenres.Add("Real-time Tactics");
+        strategy_SubGenres.Add("Tower Defense");
+        strategy_SubGenres.Add("Turn-based Strategy");
+        strategy_SubGenres.Add("Turn-based Tactics");
+        strategy_SubGenres.Add("Wargame");
+        strategy_SubGenres.Add("Grand Strategy Wargame");
+    }
+    public List<string> Get_Strategy_Subgenres()
+    {
         return strategy_SubGenres;
     }
 
-    public string[] Get_Sports_Subgenres()
+    public void Add_Sports_Subgenres()
     {
-        sports_SubGenres[0] = "Random";
-        sports_SubGenres[1] = "Racing";
-        sports_SubGenres[2] = "Sports Game";
-        sports_SubGenres[3] = "Competitive";
-        sports_SubGenres[4] = "Sports-based Fighting";
-        sports_SubGenres[5] = "Custom";
+        sports_SubGenres.Add("Random");
+        sports_SubGenres.Add("Racing");
+        sports_SubGenres.Add("Sports Game");
+        sports_SubGenres.Add("Competitive");
+        sports_SubGenres.Add("Sports-based Fighting");
+    }
 
+    public List<string> Get_Sports_Subgenres()
+    {   
         return sports_SubGenres;
     }
 
-    public string[] Get_Casual_Subgenres()
+    public void Add_Casual_Subgenres()
     {
-        sports_SubGenres[0] = "Random";
-        sports_SubGenres[1] = "Idle game";
-        sports_SubGenres[2] = "Bejewelled clone";
-        sports_SubGenres[3] = "Pet game";
-        sports_SubGenres[4] = "Gacha game";
-        sports_SubGenres[5] = "Custom";
+        sports_SubGenres.Add("Random");
+        sports_SubGenres.Add("Idle game");
+        sports_SubGenres.Add("Bejewelled clone");
+        sports_SubGenres.Add("Pet game");
+        sports_SubGenres.Add("Gacha game");
+    }
 
+    public List<string> Get_Casual_Subgenres()
+    {
         return casual_SubGenres;
     }
 }
--- Assets/Scripts/UI_Functionality.cs
+++ Assets/Scripts/UI_Functionality.cs
@@ -14,72 +14,224 @@ public class UI_Functionality : MonoBehaviour
     [Header("Game Objects")]
     [SerializeField] GameObject genre1;
     [SerializeField] GameObject genre2;
-
-    [Header("Input Fields")]
-    [SerializeField] GameObject custom_input_1;
-    [SerializeField] GameObject custom_input_2;
-
+    [SerializeField] GameObject subGenre1;
+    [SerializeField] GameObject subGenre2;
+    
     [Header("Dropdown Lists")]
     [SerializeField] TMP_Dropdown dropdown_1;
     [SerializeField] TMP_Dropdown dropdown_2;
+    [SerializeField] TMP_Dropdown subGenre1_dropdown;
+    [SerializeField] TMP_Dropdown subGenre2_dropdown;
+
+    [Header("Scripts")]
+    [SerializeField] SubGenres subGenres;
+
+    List<string> action_subGenre_Selection;
+    List<string> adventure_subGenre_Selection;
+    List<string> rhythm_subGenre_Selection;
+    List<string> puzzle_subGenre_Selection;
+    List<string> rpg_subGenre_Selection;
+    List<string> strategy_subGenre_Selection;
+    List<string> sim_subGenre_Selection;
+    List<string> sports_subGenre_Selection;
+    List<string> casual_subGenre_Selection;
+
+    bool added_items = false;
     void Start()
     {
-       
+      action_subGenre_Selection = subGenres.Get_Action_Subgenres();
+      adventure_subGenre_Selection = subGenres.Get_Adventure_Subgenres();
+      rhythm_subGenre_Selection = subGenres.Get_Rhythm_Subgenres(); 
+      puzzle_subGenre_Selection = subGenres.Get_Puzzle_Subgenres();
+      rpg_subGenre_Selection = subGenres.Get_RPG_Subgenres();
+      strategy_subGenre_Selection = subGenres.Get_Strategy_Subgenres();
+      sim_subGenre_Selection = subGenres.Get_Sim_Subgenres();
+      sports_subGenre_Selection = subGenres.Get_Sports_Subgenres();
+      casual_subGenre_Selection = subGenres.Get_Casual_Subgenres();
     }
 
     
     void Update()
     {
         Toggle_always_active();
+        Toggle1_Functionatlity();
+        Toggle2_Functionatlity();
+    }
+    
+    void Toggle1_Functionatlity()
+    {
         if (toggle1.isOn)
         {
             Toggle_One_on();
+            subGenre2.SetActive(false);
+            Dropdown_1_Functionality();
+            
 
-            if(dropdown_1.options[dropdown_1.value].text == "Custom") // If toggle 1 is on and the value is set to 'Custom' then the custom text input field will appear.
-            {
-                custom_input_1.SetActive(true);
-            }
-            else
-            {
-                custom_input_1.SetActive(false);
-            }
         }
-        
-        if (toggle2.isOn)
+    }
+
+    private void Dropdown_1_Functionality() // Sets the functionality of drop down 1
+    {
+        if (dropdown_1.options[dropdown_1.value].text == "Random")
         {
-            Toggle_Two_on();
+            subGenre1.SetActive(false); // Hides the subgenre options if Random is selected.
 
-            if (dropdown_1.options[dropdown_1.value].text == "Custom") // If toggle 1 and 2 is on and the value is set to 'Custom' then the custom text input field will appear.
-            {
-                custom_input_1.SetActive(true);
-            }
-            else
-            {
-                custom_input_1.SetActive(false);
-            }
+        }
+        else
+        {
+            subGenre1.SetActive(true);
+            Subgenre1_Input_Options();
+           
+        }
+    }
 
-            if (dropdown_2.options[dropdown_2.value].text == "Custom")
+    private void Subgenre1_Input_Options() // if custom is selected, it will reveal the custom field, else it will hide it.
+    {
+ 
+        if (dropdown_1.options[dropdown_1.value].text == "Action")
+        {
+            if (!added_items)
             {
-                custom_input_2.SetActive(true);
-            }
-            else
-            {
-                custom_input_2.SetActive(false);
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(action_subGenre_Selection);
+                added_items = true;
             }
         }
 
-        
+        else if (dropdown_1.options[dropdown_1.value].text == "Adventure")
+        {
+            if (!added_items)
+            {
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(adventure_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "Rhythm")
+        {
+            if (!added_items)
+            {
+               
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(rhythm_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "Puzzle")
+        {
+            if (!added_items)
+            {
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(puzzle_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "RPG")
+        {
+            if (!added_items)
+            {
+               
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(rpg_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "Strategy")
+        {
+            if (!added_items)
+            {
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(strategy_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "Simulation")
+        {
+            if (!added_items)
+            {
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(sim_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "Sports")
+        {
+            if (!added_items)
+            {
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(sports_subGenre_Selection);
+                added_items = true;
+            }
+        }
+
+        else if (dropdown_1.options[dropdown_1.value].text == "Casual")
+        {
+            if (!added_items)
+            {
+                
+                subGenre1_dropdown.ClearOptions();
+                subGenre1_dropdown.AddOptions(casual_subGenre_Selection);
+                added_items = true;
+            }
+        }
+        added_items = false;
 
     }
 
+    
+
+    void Toggle2_Functionatlity()
+    {
+        if (toggle2.isOn)
+        {
+            Toggle_Two_on();
+            
+            
+            if (dropdown_1.options[dropdown_1.value].text == "Random" && dropdown_2.options[dropdown_2.value].text == "Random")
+            {
+                subGenre1.SetActive(false);
+                subGenre2.SetActive(false);
+            }
+
+            else if(dropdown_1.options[dropdown_1.value].text == "Random")
+            {
+                subGenre1.SetActive(false);
+            }
+
+            else if (dropdown_2.options[dropdown_2.value].text == "Random")
+            {
+                subGenre2.SetActive(false);
+            }
+
+            else
+            {
+                subGenre1.SetActive(true);
+                subGenre2.SetActive(true);
+            }
+        }
+    }
     public void Toggle_One_on()
     {
         genre2.SetActive(false); // Hides genre2 game object if only '1 genre' is toggled.
+        
     }
 
     public void Toggle_Two_on()
     {
         genre2.SetActive(true); //Shows genre2 game object if '2 genres' is toggled.
+        
     }
 
     public void Toggle_always_active() // Makes sure one of the toggles is always on.
